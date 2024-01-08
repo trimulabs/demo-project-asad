5fbf11341c3d61aa659d859369b2865b0f9b5e14
asad-codingcrafts <155450120+asad-codingcrafts@users.noreply.github.com>
2024-01-08 15:08:49 +0500

    code improved.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-import { Box } from '@mui/material'
 import SettingsTabs from '@/components/SettingsTabs'
 
 export default function Home() {
-  return (
-    <Box sx={{ background: 'secondary' }}>
-      <SettingsTabs />
-    </Box>
-  )
+  return <SettingsTabs />
 }
